e56fdb6f825e517e66833e24fbb4c65eb524922a
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-23 05:40:10 -0500

    http added

--- http.js
+++ http.js
@@ -1,7 +1,9 @@
 const http= require('http');
-const server = http.createServer();
-server.on('connection',()=>{
-	console.log('new devise connected lol')
-})
+const server = http.createServer((req, res)=>{
+	res.write('hello')
+	res.write('world')
+	res.end();
+});
+
 server.listen(3000);
 console.log("listining on port 3000")
