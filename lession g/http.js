754f96296ad74e478cba99aff6fe9f60454c1d02
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-23 14:43:53 -0500

    updated

--- lession g/http.js
+++
@@ -1,9 +0,0 @@
-const http= require('http');
-const server = http.createServer((req, res)=>{
-	res.write('hello')
-	res.write('world')
-	res.end();
-});
-
-server.listen(3000);
-console.log("listining on port 3000")
