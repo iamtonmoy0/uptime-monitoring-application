d5af4ecd8c0d976457d3e646e26a1a538d49f104
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-26 15:59:09 -0500

    routes added

---
+++ handlers/routeHandlers/sampleHandlers.js
@@ -0,0 +1,7 @@
+const handler={};
+
+handler.sampleHandler=()=>{
+	console.log('Sample')
+}
+
+module.exports = handler;
---
+++ routes.js
@@ -0,0 +1,8 @@
+ const {sampleHandler}=require('./handlers/routeHandlers/sampleHandlers')
+ 
+ 
+ const routes={
+	'sample':sampleHandler,
+};
+
+module.exports=routes;
