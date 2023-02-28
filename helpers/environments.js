e017ea52f0c61ad766126b80523b64400db5c11f
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-28 18:16:34 -0500

    env exported

--- helpers/environments.js
+++ helpers/environments.js
@@ -11,3 +11,15 @@ environments.production={
 	port:5000,
 	envName:'production'
 };
+
+// determine which env was passed
+const currentEnvironment = typeof(process.env.NODE_ENV)==='string'? process.env.NODE_ENV:'staging';
+// export corresponding environment object
+
+const environmentToExport =
+typeof environments[currentEnvironment]==='object'
+? environments[currentEnvironment] 
+:environments.staging;
+
+// export
+module.exports=environmentToExport;
