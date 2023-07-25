8dbd588b4f0037bee03069d764803b0fed0472bf
iamtonmoy0 <tonmoy2code@gmail.com>
2023-07-25 08:03:55 -0400

    feat: server created

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+*Title: Uptime-Monitoring-Application
+*Description:A RESTFul API to monitor up/down time of user defined links
+*Author:Tonmoy(github.com/iamtonmoy0)
+*Date: 25-07-2023
+*/
+
+//dependencies
+const http=require('http')
+require('colors')
+
+//  app object -scaffold
+const app={}
+
+// config
+app.config={
+	port:3000,
+}
+// create server
+app.createServer=()=>{
+	const server=http.createServer(app.handleReqRes)
+	server.listen(app.config.port,()=>{console.log(`App is listening on port : ${app.config.port}`.yellow.bold)})
+}
+// handle request and response
+app.handleReqRes=(req,res)=>{
+	//sending response
+	res.end('hello world')
+
+}
+
+// starting the server
+ app.createServer()
